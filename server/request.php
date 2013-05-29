4a92adc157c56333c01d9962dfb25b3ced68be6b
Alberto <alberto.liu.project@gmail.com>
2013-05-29 16:52:52 +0200

    Modified request.php to handle 1 tweet request

--- server/request.php
+++ server/request.php
@@ -2,7 +2,7 @@
 /* require the user as the parameter */
 //http://localhost:8080/sample1/webservice1.php?user=1
 ini_set('display_errors', 1);
-define('__ROOT__', dirname(dirname(__FILE__)));
+define('__ROOT__', dirname(__FILE__));
 require_once(__ROOT__.'/utils/config.php'); 
 require_once (__ROOT__.'/dbQuery/db_pendingTweets_functions.php');
 require_once (__ROOT__.'/utils/utilsLog.php');
@@ -12,21 +12,22 @@ $dbPending = new DB_pendingTweets_Functions();
 
 if(isset($_GET['id_twt']) ) {
   
-  $format = strtolower($_GET['format']) == 'json' ? 'json' : 'xml'; //xml is the default
+  //$format = strtolower($_GET['format']) == 'json' ? 'json' : 'xml'; //xml is the default
   //TODO:usar un array the id_twt para soportar peticiones multiples
   $id_twt = intval($_GET['id_twt']); 
   // Get tweet requested
   $result=$dbPending->getPendingTweet($id_twt);
   /* create  array of the records */
   $tweets = array();
-  if(mysqli_num_rows($result)) {
-    while($tweet = mysqli_fetch_assoc($result)) {
-      $Arraytweets[] = array('tweet'=>$tweet);
+  $NumTweets=mysqli_num_rows($result);
+  if($NumTweets) {
+    while($tweets = mysqli_fetch_assoc($result)) {
+      $Arraytweets[] = array('tweet_response'=>$tweet);
     }
   }
   //TODO:create output to be sent in json to device
   //algorithm START
-  if($format == 'json') {
+  if($NumTweets) {
   	header('HTTP/1.1 200 OK');
     header('Content-type: application/json');
     echo json_encode(array('tweet'=>$Arraytweets));
